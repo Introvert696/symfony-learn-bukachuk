e0d32a15109f22ec762b9e2bf5c18239e1127040
Ivan Filatov <introvert696@yandex.ru>
2025-01-13 12:17:35 +0300

    lesson 4

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,32 +2,15 @@
 
 namespace App\Controller;
 
-use App\Entity\Blog;
-
-use App\Repository\BlogRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'blog_default')]
-    public function index(EntityManagerInterface $em,BlogRepository $blogRepository): Response
+    public function index(): Response
     {
-        $blog = $blogRepository->findOneBy(['id'=>2]);
-        $em->refresh($blog);
-
-        dd($blog);
-        $blog = (new Blog())
-            ->setTitle('title')
-            ->setDescription('Desctr')
-            ->setText('text')
-        ;
-        $em->persist($blog);
-        $em->flush();
-
-        return $this->render('index.html.twig', []);
+        return $this->redirectToRoute('app_blog_index');
     }
 }
